Fix masking attention

attentions.py
@@ -73,7 +73,7 @@ class Attention(nn.Module):
             attention_hidden_state, processed_memory, attention_weights_cat)
 
         if mask is not None:
-            alignment.data.masked_fill_(mask, self.score_mask_value)
+            alignment.masked_fill(mask, self.score_mask_value)
 
         attention_weights = F.softmax(alignment, dim=1)
         attention_context = torch.bmm(attention_weights.unsqueeze(1), memory)

decoder.py
@@ -214,7 +214,7 @@ class Decoder(nn.Module):
 
         while True:
             decoder_input = self.prenet(decoder_input)
-            window_mask = get_window_mask(
+            window_mask = ~get_window_mask(
                 prev_alignment=self.attention_weights,
                 window_backward=2,
                 window_forward=5